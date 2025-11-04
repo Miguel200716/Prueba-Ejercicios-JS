35e3cfad4b2f864623252eefa315da2a7e8378aa
Miguel <elaquarishd@gmail.com>
2025-11-04 09:52:50 +0100

    EJERCICIOS REPASO EXAMEN

---
+++ Ej1.js
@@ -0,0 +1,14 @@
+
+const prompt=require("prompt-sync")();
+
+/*
+1. Desarrollar un algoritmo que permita ingresar la cantidad de hijos de N personas de una empresa,
+ donde ninguno de ellos es pareja de otro, validando que este valor no sea negativo. Se declararán 
+ las variables junto con su tipo y ámbito bloque.
+
+Por tanto, se pedirá el número de personas, cada pareja sólo cuenta una,
+ y a continuación, se solicitará el número de hijos por cada persona.
+
+ Luego mostrar un cuadro resumen con la cantidad de personas que tienen hijos y los que no. 
+ Además, mostrar el promedio de hijos por persona, número máximo de hijos, el número mínimo de hijos.
+*/
