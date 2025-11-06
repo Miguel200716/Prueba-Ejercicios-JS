6648e8afd11b61fe7cf061d083544fe7a9fd1bc7
Miguel <elaquarishd@gmail.com>
2025-11-06 10:20:52 +0100

    Ejercicios 1,2,3

--- Ej2.js
+++ Ej2.js
@@ -1,1 +1,12 @@
 const prompt=require("prompt-sync")();
+
+let n=prompt("Dame la altua de la piramide");
+n=Number(n);
+for(let i=0;i<n;i++){
+    for(let j;j<n;j++){
+         console.log(" ");
+         
+    }
+    process.stdout.write("*");
+ 
+}
--- Ej3.js
+++ Ej3.js
@@ -1,1 +1,45 @@
 const prompt=require("prompt-sync")();
+
+/*El  juego de adivinar un número. El valor a adivinar lo tendremos puesto en una variable el valor
+que nosotros queramos.
+
+El número será entre 1 y 100. El programa debe dar pistas de si el número a adivinar es mayor o menor
+que el introducido.
+
+Tendremos que hacer los siguientes subprocesos (funciones):
+
+leerNumero(): Pide un numero y hasta que el usuario no escribe un valor entre 1 y 100,  
+vuelve a pedir el valor.
+
+comprobarValor(numeroUsuario, numeroCorrecto): comprueba si el número es correcto; éste  
+devuelve un número que puede ser:  0: los dos numeros son iguales  1: el numeroUsuario es mayor que 
+el numeroCorrecto  -1: el numeroUsuario es menor que el numeroCorrecto.*/
+
+let numeroSecreto = 62;
+solicitarNumero(numeroSecreto);
+
+function solicitarNumero(numeroSecreto) {
+    
+    do {
+       let intento = prompt('Introduce un número entre 1 y 100: ');
+        intento = Number(intento); 
+        if (intento > 0 && intento < 101) {
+            evaluarIntento(intento, numeroSecreto);
+        } else {
+            console.log('Tiene que ser entre 1 y 100');
+        }
+    } while (intento =! numeroSecreto);
+}
+
+function evaluarIntento(intento, numeroSecreto) {
+    if (intento === numeroSecreto) {
+        console.log(`Bien lo has conseguido`);
+        
+    } else if (intento < numeroSecreto) {
+        console.log(`El ${intento} es menor que el número secreto`);
+        solicitarNumero(numeroSecreto);
+    } else {
+        console.log(`El ${intento} es mayor que el número secreto`);
+        solicitarNumero(numeroSecreto);
+    }
+}
